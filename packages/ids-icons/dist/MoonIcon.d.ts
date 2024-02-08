d08a6f4ceb95ce1a3a16c8d31e939d5c818980b5
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-08 19:33:09 +0100

    refactor(ids-icons): delete dist

--- packages/ids-icons/dist/MoonIcon.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-declare const MemoForwardRef: React.MemoExoticComponent<React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, "ref"> & React.RefAttributes<SVGSVGElement>>>;
-export default MemoForwardRef;
