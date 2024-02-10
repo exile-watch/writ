580b6580b36bc597cf73eb3b9d19e25180d9d133
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-10 10:48:38 +0100

    feat(providers): add WritProvider

---
+++ packages/writ-react/src/providers/WritProvider.tsx
@@ -0,0 +1,16 @@
+import '@mantine/core/styles.css';
+import {createTheme, MantineProvider} from "@mantine/core";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+const WritProvider = ({children}) => {
+  return (
+    <MantineProvider theme={theme}>
+      {children}
+    </MantineProvider>
+  );
+};
+
+export default WritProvider;
