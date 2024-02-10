8407552fd3279bf02341d5cf194f4c9166a73e7d
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-10 21:53:36 +0100

    chore(packages): swap npm in favor of pnpm and update release workflow

--- packages/writ-react/src/components/Heading/Heading.tsx
+++ packages/writ-react/src/components/Heading/Heading.tsx
@@ -1,28 +1,15 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import cx from 'classnames';
+import {Title as MantineTitle, TitleProps} from '@mantine/core'
 
 import styles from './Heading.module.scss';
 
-type HeadingProps = {
-  as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
-  children: ReactNode;
-  className?: string;
-};
-
-const Heading = ({ as: T = 'h3', children = null, className = '', ...props }: HeadingProps) => {
-  const fontSize = {
-    [styles.h1]: T === 'h1',
-    [styles.h2]: T === 'h2',
-    [styles.h3]: T === 'h3',
-    [styles.h4]: T === 'h4',
-    [styles.h5]: T === 'h5',
-  };
-
+const Title = ({ children, ...props}: TitleProps) => {
   return (
-    <T className={cx(styles.heading, fontSize, className)} {...props}>
+    <MantineTitle {...props}>
       {children}
-    </T>
+    </MantineTitle>
   );
 };
 
-export {Heading};
+export {Title};
