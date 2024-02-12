fa23a519902f26141a1a76af9376aaf09e385bea
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-12 19:42:47 +0100

    refactor(components): export mantine for the time being

--- packages/writ-react/src/components/index.ts
+++ packages/writ-react/src/components/index.ts
@@ -1,10 +1,12 @@
-export * from './Badge/Badge';
-export * from './Button/Button';
-export * from './Card/Card';
-export * from './Title/Title';
-export * from './Input/Input';
-export * from './InputGroup/InputGroup';
+// export * from './Badge/Badge';
+// export * from './Button/Button';
+// export * from './Card/Card';
+// export * from './Title/Title';
+// export * from './Input/Input';
+// export * from './InputGroup/InputGroup';
 export * from './Message/Message';
 export * from './MissingContent/MissingContent';
-export * from './PageLoader/PageLoader';
+// export * from './PageLoader/PageLoader';
 export * from './ThemeSwitcher/ThemeSwitcher';
+
+export * from '@mantine/core'
